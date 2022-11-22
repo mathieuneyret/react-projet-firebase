025925a37dc4744e7b3f513bd05dfcbd73f16160
mathieuneyret <neyret.mathieu69@gmail.com>
2022-11-22 16:28:39 +0100

    Resolve dotenv and login firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes, Switch} from "react-router-dom";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {
-    GoogleAuthProvider,
     getAuth,
-    signInWithPopup,
     signInWithEmailAndPassword,
     createUserWithEmailAndPassword,
     sendPasswordResetEmail,
@@ -10,43 +8,21 @@ import {
 } from "firebase/auth";
 import {
     getFirestore,
-    query,
-    getDocs,
     collection,
-    where,
     addDoc,
 } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+console.log(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const googleProvider = new GoogleAuthProvider();
-const signInWithGoogle = async () => {
-    try {
-        const res = await signInWithPopup(auth, googleProvider);
-        const user = res.user;
-        const q = query(collection(db, "users"), where("uid", "==", user.uid));
-        const docs = await getDocs(q);
-        if (docs.docs.length === 0) {
-            await addDoc(collection(db, "users"), {
-                uid: user.uid,
-                name: user.displayName,
-                authProvider: "google",
-                email: user.email,
-            });
-        }
-    } catch (err) {
-        console.error(err);
-        alert(err.message);
-    }
-};
 const logInWithEmailAndPassword = async (email, password) => {
     try {
         await signInWithEmailAndPassword(auth, email, password);
@@ -85,7 +61,6 @@ const logout = () => {
 export {
     auth,
     db,
-    signInWithGoogle,
     logInWithEmailAndPassword,
     registerWithEmailAndPassword,
     sendPasswordReset,
